b4f5ef2889b15e27ff731a56f8993719fec5a532
Renan <souza.rnn@gmail.com>
2021-01-05 01:49:44 -0300

    GET /orders

--- backend/src/main/java/br/com/somenteparaestudosetestes/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/br/com/somenteparaestudosetestes/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,17 @@
 package br.com.somenteparaestudosetestes.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.somenteparaestudosetestes.dsdeliver.entities.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	//Método para listar pedidos pendentes dos mais antigos para os mais novos
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ "WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
+	
 }
