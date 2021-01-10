946e73a3ac443b8132c01277e7a8a9d900370f29
unknown <souza.rnn@gmail.com>
2021-01-10 12:31:34 -0300

    Ajustes para deploy

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,8 +1,7 @@
 import { OrderPayload } from './Orders/types';
 import axios from "axios";
 
-//const API_URL = 'https://renan-sds2-projeto-dsdeliver.herokuapp.com';
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://renan-sds2-projeto-dsdeliver.herokuapp.com';
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
