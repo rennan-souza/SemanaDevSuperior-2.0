faf1d991eb709231b9b1b6c302a6e6879fb2edd9
unknown <souza.rnn@gmail.com>
2021-01-10 03:00:23 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,8 @@
+import { OrderPayload } from './Orders/types';
 import axios from "axios";
 
-const API_URL = 'https://renan-sds2-projeto-dsdeliver.herokuapp.com';
+//const API_URL = 'https://renan-sds2-projeto-dsdeliver.herokuapp.com';
+const API_URL = 'http://localhost:8080';
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
@@ -9,4 +11,8 @@ export function fetchProducts() {
 
 export function fetchLocalMapBox(local: string) {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayload) {
+    return axios.post(`${API_URL}/orders`, payload)
 }
